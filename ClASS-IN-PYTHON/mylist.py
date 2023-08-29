ea15ea5bdab12dc2ab61dc5d28f4464fb0c88751
Trying-1 <tryingbait@gmail.com>
2023-08-29 22:20:31 +0530

    added searhInList and deleteNode method to list

--- ClASS-IN-PYTHON/mylist.py
+++ ClASS-IN-PYTHON/mylist.py
@@ -22,9 +22,46 @@ class List:
         while  n is not None:
             print(n.value)
             n=n.next
-        
+    def countList(self):
+        n=self.head
+        count=0
+        while n is not None:
+            n=n.next
+            count+=1
+        print(f"number of element in the lis:{count}")
+    def searchInList(self,data):
+        n=self.head
+        found = False
+        while n is not None:
+            if(n.value==data):
+                found=True
+                break
+            else:
+                n=n.next
+        if found:
+            print(f" {data} the was found in the list")
+            return True
+        else:
+            print(f" {data} the was  not found in the list")
+            return False
+    def deleteNode(self,data):
+        if self.searchInList(data):
+            n=self.head
+            if n.value==data:
+                self.head=n.next
+                return
+            else:
+                while n.value is not data:
+                    prevNode= n
+                    n=n.next
+                nextNode=n.next
+                prevNode.next=nextNode   
+        else:
+            print("data not found to delete")
+    
+                          
 mylist=List()
 mylist.append(5)
 mylist.append(9)
 mylist.append(10)
-mylist.displayList()
+
